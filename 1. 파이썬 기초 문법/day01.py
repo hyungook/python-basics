14b6f15b3c7bf9ad8292b0729f69f3a2f9c789ba
오형욱 <nenonenoberneno@gmail.com>
2021-04-08 22:28:42 +0900

    python basics study

---
+++ 1. 파이썬 기초 문법/day01.py
@@ -0,0 +1,59 @@
+'''
+Day 01
+
+변수명 정하기
+1) 영문과 숫자, _로 이루어진다.
+2) 대소문자를 구분한다.
+3) 문자나, _로 시작한다.
+4) 특수문자를 사용하면 안된다.(if, for 등)
+
+'''
+
+a=1
+A=2
+A1=3
+#2b=4
+print(a,A,A1)
+a,b,c=3,2,1
+print(a,b,c)
+
+#값 교환
+a,b=10, 20
+print(a,b)
+a,b=b,a
+print(a,b)
+
+#변수 타입
+a=12345
+print(type(a))
+a=12.123456789123456789
+# 실수형은 8byte를 넘어가면 데이터가 소실된다.
+print(a)
+print(type(a))
+a='student'
+print(a)
+print(type(a))
+
+
+
+# 출력 방식
+
+print("number")
+a,b,c=1,2,3
+print(a,b,c)
+print("number :",a,b,c)
+# seperate = sep=''
+print(a,b,c, sep='')
+print(a,b,c, sep='\n') # 줄바꿈
+
+print(a, end=' ') # 줄바꾸기 없앰
+print(b, end=' ')
+print(c)
+
+
+
+
+
+
+
+
---
+++ 1. 파이썬 기초 문법/day02.py
@@ -0,0 +1,38 @@
+'''
+Day 02
+
+변수 입력과 연산자
+'''
+
+
+
+# a=input("숫자를 입력하세요 :")
+# print(a)
+
+# split
+'''
+a,b=input("숫자를 입력하세요 :").split()
+a=int(a)
+b=int(b)
+print(a+b)
+#print(type(a))
+
+
+
+a,b=map(int,input("숫자를 입력하세요: ").split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) # a 를 b로 나눈 몫
+print(a%b) # a를 b로 나눈 나머지
+print(a**b) # 제곱
+
+'''
+
+
+# type은 형이 다른 연산을 하면 더 넓은, 더 큰 범위로 출력값이 나온다.
+a=4.3 #실수
+b=5 #정수
+c=a+b #실수
+print(type(c))
